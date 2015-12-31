1483ad642741c24282c40665406a03af3c6ab9e5
Eric Basti <ericjbasti@gmail.com>
2015-12-30 20:34:23 -0500

    updates

--- stats.js
+++ stats.js
@@ -6,7 +6,7 @@ var STATS = new Studio.Plugin({
 		show_text: true,
 		clear_mode: 'cover', // modes : cover, erase
 		position: 0,
-		refresh: .5,
+		refresh: .25,
 	},
 	init: function(a) { // lets build out a canvas for the stats
 		this.buffer = document.createElement('canvas');
@@ -16,6 +16,7 @@ var STATS = new Studio.Plugin({
 		this.buffer.width = this.options.width;
 		this.buffer.height = this.options.height;
 		this.half = this.buffer.height / 2;
+		this.old_memory = 0;
 		if (this.options.show_text) {
 			this.half = this.buffer.height
 		}
@@ -58,16 +59,26 @@ var STATS = new Studio.Plugin({
 			this._tick = 0;
 		}
 		this.displayFPS(this.buffer.ctx);
+		// if(window.performance){
+		// 	if(window.performance.memory){
+		// 		this.drawMemory(this.buffer.ctx);
+		// 	}
+		// }
 		if (!this.options.external) {
 			a.ctx.drawImage(this.buffer, 0, this.options.position);
 		}
 	},
 	cover: function(ctx) {
-		ctx.fillStyle = 'rgba(0,0,0,.3)';
+		ctx.fillStyle = 'rgba(0,0,0,.5)';
 		ctx.fillRect(0, 12 * this.options.show_text, this.buffer.width, this.buffer.height);
 	},
 	erase: function(ctx) {
 		ctx.clearRect(0, 0, this.buffer.width, this.buffer.height);
+	},
+	drawMemory: function(ctx){
+		ctx.fillStyle = '#FC00DE';
+		this.memory = window.performance.memory.usedJSHeapSize;
+		ctx.fillRect(this.step,15,2, ((this.memory* 0.000000954)-7)*2);
 	},
 	displayFPS: function(ctx) {
 		if (Studio.delta > 1000/15) {
@@ -90,7 +101,12 @@ var STATS = new Studio.Plugin({
 		ctx.fillStyle = 'rgba(0,0,0,.8)';
 		ctx.fillRect(0, 0, this.buffer.width, 12);
 		ctx.fillStyle = 'rgb(255,255,255)';
-		ctx.fillText((this._tick/this.options.refresh | 0) + ' fps / ' + Studio.draws + ' draw / ' + this._spikes + ' spikes', 2, 10);
+		if(this.memory){
+			ctx.fillText((this._tick/this.options.refresh | 0) + ' fps / ' + Studio.draws + ' draw / ' + this._spikes + ' spikes     MEM: '+(this.memory* 0.000000954).toFixed(3)+'  &  Buildup: '+(this.memory-this.old_memory), 2, 10);
+			this.old_memory = this.memory;
+		}else{
+			ctx.fillText((this._tick/this.options.refresh | 0) + ' fps / ' + Studio.draws + ' draw / ' + this._spikes + ' spikes', 2, 10);
+		}
 	}
 
 })
