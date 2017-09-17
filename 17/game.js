b7675069437e5566c6c9468ad93a71f695ecd901
Eric Basti <ericjbasti@gmail.com>
2017-09-17 11:00:39 -0400

    update 17

--- 17/game.js
+++ 17/game.js
@@ -1,11 +1,21 @@
-var marker = function( x, y, number){
+var marker = function( x, y, color){
 	this.x = x + 34
 	this.y = y + 34
 	this.color = new Studio.Color(255,255,255,.5)
+
+	var number = parseInt(color*3)
+
+	switch (number){
+		case 0 : this.color.setFromHex('#FF00BB'); break
+		case 1 : this.color.setFromHex('#FFD300'); break
+		case 2 : this.color.setFromHex('#00C4FF'); break
+		case 3 : this.color.setFromHex('#2EFF00'); break		
+	}
+	this.color.a = .5
+
 	this.hoverIn_tween = stage.createTween(this, 'quadInOut', {scaleX: 1.2, scaleY: 1.2, rotation: 45}, 300).loop(true).reflect(true);
 	this.hoverOut_tween = stage.createTween(this, 'quadOut', {scaleX: 1, scaleY: 1, rotation: 0}, 300);
 	this.tap_tween = stage.createTween(this, 'quadOut', {scaleX:.9, scaleY:.9, rotation: 0 }, 100, function(){
-		this.color.setFromHex('#135')
 	})
 }
 
@@ -14,13 +24,17 @@ Studio.inherit( marker, Studio.Rect, {width: 40, height: 40})
 marker.prototype.onHoverStart = function(){
 	stage.stopTween(this.hoverOut_tween)
 	stage.playTween(this.hoverIn_tween)
-	this.color.r = 0
+	this.color.a = 1
+	this.z = 5
+	Game._order();
 }
 
 marker.prototype.onHoverEnd = function(){
 	stage.stopTween(this.hoverIn_tween)
 	stage.playTween(this.hoverOut_tween)
-	this.color.r = 1
+	this.color.a = .5
+	this.z = 1
+	Game._order();
 }
 
 marker.prototype.onTap = function(){
@@ -41,7 +55,7 @@ var Game = new Studio.Scene({
 		
 		for(var i = 0; i!= 7; i++){
 			for(var j = 0; j!= 7; j++){
-				var mark = new marker(i*42, j*42)
+				var mark = new marker(i*42, j*42, Math.random())
 				this.addChild( mark )
 				this.addButton( mark )
 			}
