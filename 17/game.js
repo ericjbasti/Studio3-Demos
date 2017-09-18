cc10c2b6cf69f604d5b4436eea98abf285711aea
Eric Basti <ericjbasti@gmail.com>
2017-09-17 21:31:49 -0400

    17 update

--- 17/game.js
+++ 17/game.js
@@ -1,17 +1,26 @@
+var activeList = new LinkedList()
+
+var checkValue = function(who){
+	var value = 0;
+	who.each(function(a,i){
+		value+=a.number
+	})
+	alert(value)
+}
+
 var marker = function( x, y, color){
 	this.x = x + 34
 	this.y = y + 34
 	this.color = new Studio.Color(255,255,255,.5)
 
-	var number = parseInt(color*3)
+	this.number = Math.ceil(color*3)
 
-	switch (number){
-		case 0 : this.color.setFromHex('#FF00BB'); break
-		case 1 : this.color.setFromHex('#FFD300'); break
-		case 2 : this.color.setFromHex('#00C4FF'); break
-		case 3 : this.color.setFromHex('#2EFF00'); break		
+	switch (this.number){
+		case 1 : this.color.setFromHex('#FFee00'); break
+		case 2 : this.color.setFromHex('#0077FF'); break
+		case 3 : this.color.setFromHex('#FF0000'); break		
 	}
-	this.color.a = .5
+	this.color.a = .8
 
 	this.hoverIn_tween = stage.createTween(this, 'quadInOut', {scaleX: 1.2, scaleY: 1.2, rotation: 45}, 300).loop(true).reflect(true);
 	this.hoverOut_tween = stage.createTween(this, 'quadOut', {scaleX: 1, scaleY: 1, rotation: 0}, 300);
@@ -24,7 +33,7 @@ Studio.inherit( marker, Studio.Rect, {width: 40, height: 40})
 marker.prototype.onHoverStart = function(){
 	stage.stopTween(this.hoverOut_tween)
 	stage.playTween(this.hoverIn_tween)
-	this.color.a = 1
+	// this.color.a = 1
 	this.z = 5
 	Game._order();
 }
@@ -32,14 +41,23 @@ marker.prototype.onHoverStart = function(){
 marker.prototype.onHoverEnd = function(){
 	stage.stopTween(this.hoverIn_tween)
 	stage.playTween(this.hoverOut_tween)
-	this.color.a = .5
+	// this.color.a = .8
 	this.z = 1
 	Game._order();
 }
 
 marker.prototype.onTap = function(){
-	stage.stopTween(this.hoverIn_tween)
-	stage.playTween(this.tap_tween)
+	if(!this.inList){
+		this.inList = true
+		activeList.add(this)
+		this.color.a = .5
+		checkValue(activeList)
+	}else{
+		this.inList = false
+		activeList.remove(this)
+		this.color.a = 1
+		checkValue(activeList)
+	}
 }
 
 var Game = new Studio.Scene({
